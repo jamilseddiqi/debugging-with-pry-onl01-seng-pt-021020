b2dbf36adfb156456aa567602aa82e24edd426eb
Jamil Seddiqi <jamilsediq@gmail.com>
2020-03-14 21:39:05 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,6 +1,38 @@
-require pry 
-def plus_two(num)
-	num + 2
-	binding.pry 
-	num
+# require pry 
+# def plus_two(num)
+# 	num + 2
+# 	binding.pry 
+# 	num
+# end
+
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = [] 
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
 end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+end 
+puts self.all 
